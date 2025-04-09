05049d8e315aa5aa1a75c3ff08013238c056c99b
NobulPlus <chenko4me@gmail.com>
2025-04-09 15:58:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,253 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from theme-land.com/sapp/demo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 07 Apr 2025 12:19:17 GMT -->
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<!-- Title  -->
+	<title>LASHMA - App Landing Page</title>
+
+	<!-- Favicon  -->
+	<link rel="icon" href="assets/img/android-chrome-512x512.png">
+
+	<!-- ***** All CSS Files ***** -->
+
+	<!-- Style css -->
+	<link rel="stylesheet" href="assets/css/style.css">
+
+	<!-- AOS CSS -->
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+	<!-- AOS JS -->
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+</head>
+
+<body>
+	<div class="main">
+		<!-- ***** Preloader Start ***** -->
+		<div class="preloader-main">
+			<div class="preloader-wapper">
+				<svg class="preloader" xmlns="http://www.w3.org/2000/svg" version="1.1" width="600" height="200">
+					<defs>
+						<filter id="goo" x="-40%" y="-40%" height="200%" width="400%">
+							<feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+							<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -8" result="goo" />
+						</filter>
+					</defs>
+					<g filter="url(#goo)">
+						<circle class="dot" cx="50" cy="50" r="25" fill="#8731E8" />
+						<circle class="dot" cx="50" cy="50" r="25" fill="#8731E8" />
+					</g>
+				</svg>
+				<div>
+					<div class="loader-section section-left"></div>
+					<div class="loader-section section-right"></div>
+				</div>
+			</div>
+		</div>
+		<!-- ***** Preloader End ***** -->
+
+		<!-- ***** Header Start ***** -->
+		<header id="header">
+			<!-- Navbar -->
+			<nav data-aos="zoom-out" data-aos-delay="800" class="navbar gameon-navbar navbar-expand">
+				<div class="container header">
+
+					<!-- Logo -->
+					<a class="navbar-brand" href="index.html">
+						<img src="assets/img/logo/logo2.png" alt="Brand Logo" width="30%" />
+					</a>
+
+					<div class="ms-auto"></div>
+
+					<!-- Navbar Nav -->
+					<ul class="navbar-nav items mx-auto">
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link smooth-anchor" href="https://lashma.com/plan/">Plans</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link">Our Network <i class="icon-arrow-down"></i></a>
+							<ul class="dropdown-menu">
+								<li class="nav-item">
+									<a href="https://lashma.com/marketing-agents/" class="nav-link">Marketing Agents</a>
+								</li>
+								<li class="nav-item">
+									<a href="https://lashma.com/provider/" class="nav-link">View Providers</a>
+								</li>
+								<li class="nav-item">
+									<a href="https://verify.lshsportal.com:8283/" class="nav-link">LSHS Verification</a>
+								</li>
+								<li class="nav-item">
+									<a href="https://lashma.com/resources/" class="nav-link">Resources</a>
+								</li>
+								<li class="nav-item">
+									<a href="https://bit.ly/change-my-ileraeko-provider" class="nav-link">Change Hospital</a>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link smooth-anchor" href="#screenshots">About Us</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link smooth-anchor" href="#contact">Contact</a>
+						</li>
+					</ul>
+
+					<!-- Navbar Toggler -->
+					<ul class="navbar-nav toggle">
+						<li class="nav-item">
+							<a href="#" class="nav-link" data-bs-toggle="modal" data-bs-target="#menu">
+								<i class="icon-menu m-0"></i>
+							</a>
+						</li>
+					</ul>
+
+				</div>
+			</nav>
+		</header>
+		<!-- ***** Header End ***** -->
+
+		<!-- ***** Hero Section Start ***** -->
+		<section id="home" class="hero-section layout-1 has-overlay overlay-gradient">
+			<div class="container">
+				<div class="row justify-content-center align-items-center">
+					<div class="col-12 col-md-7 col-lg-6">
+						<!-- Hero Content -->
+						<div class="hero-content">
+							<h2 class="text-white">Healthcare at Your Fingertips with Ilera Eko Telemed</h2>
+							<p class="sub-heading text-white my-4">Experience next-level care with secure teleconsultations and e-diagnostics. Powered by LASHMA, Ilera-Eko Telemed brings quality healthcare to you, anytime, anywhere.</p>
+
+							<!-- Download Button -->
+							<div class="button-group download-button">
+								<a href="https://play.google.com/store/apps/details?id=com.lashma.app&hl=en_US">
+									<img src="assets/img/content/google-play.png" alt="">
+								</a>
+								<a href="#">
+									<img src="assets/img/content/app-store.png" alt="">
+								</a>
+							</div>
+							<span class="d-block fst-italic fw-light text-white mt-3">* Available on iPhone, iPad and all Android devices</span>
+						</div>
+					</div>
+					<div class="col-12 col-md-5 col-lg-6">
+						<!-- Hero Thumb -->
+						<div class="hero-thumb mx-auto" data-aos="fade-left" data-aos-delay="500" data-aos-duration="1000">
+							<img src="assets/img/content/mobile.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Shape Bottom -->
+			<div class="shape-bottom">
+				<svg viewBox="0 0 1920 310" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="svg replaced-svg">
+					<title>sApp Shape</title>
+					<desc>Created with Sketch</desc>
+					<defs></defs>
+					<g id="sApp-Landing-Page" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+						<g id="sApp-v1.0" transform="translate(0.000000, -554.000000)" fill="#FFFFFF">
+							<path d="M-3,551 C186.257589,757.321118 319.044414,856.322454 395.360475,848.004007 C509.834566,835.526337 561.525143,796.329212 637.731734,765.961549 C713.938325,735.593886 816.980646,681.910577 1035.72208,733.065469 C1254.46351,784.220361 1511.54925,678.92359 1539.40808,662.398665 C1567.2669,645.87374 1660.9143,591.478574 1773.19378,597.641868 C1848.04677,601.75073 1901.75645,588.357675 1934.32284,557.462704 L1934.32284,863.183395 L-3,863.183395" id="sApp-v1.0"></path>
+						</g>
+					</g>
+				</svg>
+			</div>
+		</section>
+		<!-- ***** Hero Section End ***** -->
+
+		<!-- ***** Counter Area Start ***** -->
+		<section class="resource-area py-5">
+			<div class="container">
+			  <div class="row gy-4 justify-content-center">
+				
+				<!-- ILERA EKO -->
+				<div class="col-12 col-sm-10 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+				  <div class="text-center p-4 border rounded shadow-sm h-100 bg-white">
+					<a href="https://ileraeko.com/" target="_blank" class="text-decoration-none d-block">
+					  <img src="assets/img/content/ileraeko.png" alt="ILERA EKO Logo" class="mb-3 img-fluid" style="max-height: 80px;">
+					  <h5 class="mb-0 text-dark">Click here to visit ILERA EKO</h5>
+					</a>
+				  </div>
+				</div>
+		  
+				<!-- EKOSHA -->
+				<div class="col-12 col-sm-10 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+				  <div class="text-center p-4 border rounded shadow-sm h-100 bg-white">
+					<a href="https://lashma.com/ekosha" target="_blank" class="text-decoration-none d-block">
+					  <img src="assets/img/content/ekosha.png" alt="EKOSHA Logo" class="mb-3 img-fluid" style="max-height: 80px;">
+					  <h5 class="mb-0 text-dark">Click here to visit EKOSHA</h5>
+					</a>
+				  </div>
+				</div>
+		  
+				<!-- LSHS Regulations -->
+				<div class="col-12 col-sm-10 col-md-6 col-lg-4" data-aos="fade-up" data-aos-delay="300">
+				  <div class="text-center p-4 border rounded shadow-sm h-100 bg-white">
+					<a href="#" target="_blank" class="text-decoration-none d-block">
+					  <img src="assets/img/content/lshs-logo.png" alt="LSHS Regulations Logo" class="mb-3 img-fluid" style="max-height: 80px;">
+					  <h5 class="mb-0 text-dark">Click here to visit LSHS Regulations</h5>
+					</a>
+				  </div>
+				</div>
+		  
+			  </div>
+			</div>
+		  </section>
+				
+		<!-- ***** Counter Area End ***** -->
+
+
+		<!--====== Scroll To Top Area Start ======-->
+		<div id="scroll-to-top" class="scroll-to-top">
+			<a href="#home" class="smooth-anchor">
+				<i class="fa-solid fa-arrow-up"></i>
+			</a>
+		</div>
+		<!--====== Scroll To Top Area End ======-->
+	</div>
+
+
+	<!-- ***** All jQuery Plugins ***** -->
+
+	<!-- jQuery(necessary for all JavaScript plugins) -->
+	<script src="assets/js/vendor/jquery.min.js"></script>
+
+	<!-- Bootstrap js -->
+	<script src="assets/js/vendor/popper.min.js"></script>
+	<script src="assets/js/vendor/bootstrap.min.js"></script>
+
+	<!-- Plugins js -->
+	<script src="assets/js/vendor/slider.min.js"></script>
+	<script src="assets/js/vendor/owl.carousel.min.js"></script>
+	<script src="assets/js/vendor/counterup.js"></script>
+	<script src="assets/js/vendor/waypoint.js"></script>
+	<script src="assets/js/vendor/aos.js"></script>
+	<script src="assets/js/vendor/wow.min.js"></script>
+	<script src="assets/js/vendor/countdown.min.js"></script>
+	<script src="assets/js/vendor/gallery.min.js"></script>
+
+	<!-- Main js -->
+	<script src="assets/js/main.js"></script>
+
+	<!-- AOS CSS -->
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+<!-- AOS JS -->
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script>
+		AOS.init();
+	  </script>
+</body>
+
+
+<!-- Mirrored from theme-land.com/sapp/demo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 07 Apr 2025 12:20:01 GMT -->
+</html>
